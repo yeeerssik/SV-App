991157a355b00577c62e41801f8accb62c7b3d23
Yers <58120353+yeeerssik@users.noreply.github.com>
2022-12-02 11:15:07 +0600

    Hide my Atlassian password

--- Server/app.ts
+++ Server/app.ts
@@ -8,7 +8,7 @@ import checkAuth from './utils/checkAuth';
 import * as UserController from './controllers/UserController';
 
 mongoose
-    .connect('mongodb+srv://yers:Vem_100802@diploma-project-cluster.sozejqp.mongodb.net/blog?retryWrites=true&w=majority')
+    .connect('mongodb+srv://yers:password@diploma-project-cluster.sozejqp.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => console.log('Connected to DB'))
     .catch((err) => console.log('DB error: ', err));
 
